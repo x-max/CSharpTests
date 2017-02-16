d43508a59c3147b9836e391fd8c92269de36022c
806590 <nemanja.maksic@gmail.com>
2017-02-16 14:14:47 +0100

    Selenium projekat komitovanje za dalju razradu

---
+++ SeleniumExecuteAutomation/SeleniumExecuteAutomation/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExecuteAutomation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://google.com");
+
+            driver.Close();
+        }
+    }
+}
