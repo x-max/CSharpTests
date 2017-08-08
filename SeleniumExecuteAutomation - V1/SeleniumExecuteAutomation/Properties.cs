d3bb56ae119199a1940365ef20441d9e348f26d6
806590 <nemanja.maksic@gmail.com>
2017-08-08 09:51:36 +0200

    Page Navigation of POM in Selenium with C# -- Part 9

---
+++ SeleniumExecuteAutomation - V1/SeleniumExecuteAutomation/Properties.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExecuteAutomation
+{
+    //Important for enum to be outside of class so it is visible throughout whole solution
+    enum PropertyType
+    {
+        Id,
+        Name,
+        CssName,
+        LinkText,
+        ClassName
+    }
+
+    class Properties
+    {
+        public static  IWebDriver driver{ get; set; }
+    }
+}
--- SeleniumExecuteAutomation/SeleniumExecuteAutomation/Program.cs
+++ SeleniumExecuteAutomation/SeleniumExecuteAutomation/Program.cs
@@ -1,10 +1,14 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumExecuteAutomation
 {
@@ -12,10 +16,37 @@ namespace SeleniumExecuteAutomation
     {
         public static void Main(string[] args)
         {
+            //IWebDriver driver = new InternetExplorerDriver(@"C:\Users\806590\Source\Repos\CSharpTests\SeleniumExecuteAutomation\packages\Selenium.WebDriver.IEDriver.3.2.0\driver");
+            //InternetExplorerOptions options = new InternetExplorerOptions();
+            //options.EnablePersistentHover = false;
+            //IWebDriver driver = new InternetExplorerDriver(@"C:\Users\806590\Source\Repos\CSharpTests\SeleniumExecuteAutomation\packages\Selenium.WebDriver.IEDriver.3.2.0\driver", options);
             IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+            var ActionToolsButton = driver.FindElement(By.XPath("//span[@id='Automation Tools']"));
+            var Selenium = driver.FindElement(By.CssSelector("a span#Selenium"));
+            var SeleniumWebDriver = driver.FindElement(By.CssSelector("a[href='seleniumwebdriver.html'] span"));
 
-            driver.Navigate().GoToUrl("https://google.com");
+            By s1 = By.CssSelector("a span#Selenium");
+            By s2 = By.CssSelector("//span[@id='Automation Tools']");
+            By s3 = By.CssSelector("a[href='seleniumwebdriver.html'] span");
+            //;
+            Actions action = new Actions(driver);
+            action.MoveToElement(ActionToolsButton).Perform();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(ExpectedConditions.ElementExists((s1)));
+            action.MoveToElement(Selenium).Perform();
+            Thread.Sleep(2000);
+            var x = action.MoveToElement(SeleniumWebDriver);
+            x.Click().Build().Perform();
+            
+            Thread.Sleep(2000);
+            if (driver.Url == "http://executeautomation.com/demosite/seleniumwebdriver.html")
+            {
+                Console.WriteLine("The Page has opened successfully");
+            }
 
+            else Console.WriteLine("Page is not opened!!!!");
+          
+            Thread.Sleep(5000);
             driver.Close();
         }
     }
